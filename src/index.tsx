8821086fdf49527033c824eff66ea1aa82e790af
builtbyjosh <pc.refuge@gmail.com>
2021-09-06 23:27:08 -0400

    add esbuild assembly to public, setup ref

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,38 @@
 import ReactDOM from 'react-dom'
+import { useState, useEffect, useRef } from 'react'
+import * as esbuild from 'esbuild-wasm'
 
 const App = () => {
-  return <h1>Hi</h1>
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+  const ref = useRef<any>()
+
+  const onClick = () => {
+    if(!ref.current){
+      return;
+    }
+    console.log(ref.current)
+  }
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    })
+    
+  }
+
+  useEffect(() => {
+    startService()
+  }, [] )
+
+  return <div>
+    <textarea value={input} onChange={e => setInput(e.target.value)} > </textarea>
+    <div>
+      <button onClick={onClick} >Submit</button>
+    </div>
+    <pre>{code}</pre>
+  </div>
 }
 
 ReactDOM.render(
