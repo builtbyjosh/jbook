47058c5fe276839481defb9c5e4359f14127a4d6
builtbyjosh <pc.refuge@gmail.com>
2021-09-22 21:44:00 -0500

    refactor plugin to handle nested packages

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -14,7 +14,7 @@ export const unpkgPathPlugin = () => {
         if(args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            path: new URL(args.path, args.importer + '/').href
+            path: new URL(args.path,'https://unpkg.com' + args.resolveDir + '/').href
           }
         }
 
@@ -36,15 +36,16 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('medium-test-pkg');
+              const message = require('react');
               console.log(message);
             `,
           };
         }
-        const { data } = await axios.get(args.path)        
+        const { data, request } = await axios.get(args.path)               
         return {
           loader: 'jsx',
           contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
         }
       });
     },
